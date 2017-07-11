168525f99ed92cf806d242183d04d7d67a97b851
wchiou1 <starcraft1202@gmail.com>
2017-07-11 13:47:58 -0400

    Created Incubator Class

---
+++ src/TreeUI/Incubator.java
@@ -0,0 +1,15 @@
+package TreeUI;
+
+/**
+ * This class will handle the creation of panel trees
+ * It will be able to create a new panel, add UIElements and connect them together.
+ * @author Wesley
+ *
+ */
+public class Incubator{
+	public Incubator(){}
+	private HashTable<>
+	public addPanel(){
+		
+	}
+}
