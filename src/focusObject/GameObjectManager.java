84f0a3d3177a333f1cb3aa70634f1936055a5d6c
wchiou1 <starcraft1202@gmail.com>
2018-04-22 21:20:45 -0400

    Added gameobjectmanager

---
+++ src/focusObject/GameObjectManager.java
@@ -0,0 +1,6 @@
+package focusObject;
+
+//This class will negotiate between gameobjects and the incubator(ei, prevent game objects from creating editor items for instance)
+public class GameObjectManager{
+	
+}
